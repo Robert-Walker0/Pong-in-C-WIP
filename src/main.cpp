23900a021ecfbde4d19584911e9da3441ccc3d44
RobertWalker0 <algorithmicrealmist@gmail.com>
2023-08-07 12:55:32 -0700

    Feat: Program options with a window now.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+/*
+TODO: Create the Paddles
+TODO: Create the Ball
+TODO: Create the scoring system
+*/
+int windowWidth = 500;
+int windowHeight = 500;
+const char* windowName = "Pong";
+
 int main()
 {
+    GLFWwindow* gameScreen;
     if(!glfwInit())
-    {
-        std::cout << "OpenGL is not on the system." << std::endl;
-        system("pause");
         return -1;
-    } else {
-        std::cout << "OpenGL is on the system" << std::endl;
-        system("pause");
-        return 0;
+    
+    gameScreen = glfwCreateWindow(windowWidth, windowHeight, windowName, NULL, NULL);
+    if(!gameScreen)
+    {
+        glfwTerminate();
+        return -1;
     }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(gameScreen);
+    while(!glfwWindowShouldClose(gameScreen))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(gameScreen);
+        glfwPollEvents();
+    }
+    glfwTerminate();
 }
