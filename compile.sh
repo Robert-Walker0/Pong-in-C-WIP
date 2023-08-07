c5228cf6d00cef1e848b8ad99124dd54282e24f4
RobertWalker0 <algorithmicrealmist@gmail.com>
2023-08-06 17:27:45 -0700

    The compiling file finally compiles an executable
    The original placeholder bin has been replaced with the full path of the
    where the glfw3.dll is being held. Gdi32 was added here as well, without
    it, glfwInit sends a bunch of errors. The executable didn't need to be
    taken from the src file since it compiled to the root dir, so it was
    just sent to the bin normally.

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-g++ -I"deps/GLFW/include" src/main.cpp -o Pong.exe -L"deps/..." -lglfw3.lib
+g++ -I"deps/GLFW/include" src/main.cpp -o Pong.exe -L"deps/GLFW/lib-mingw-w64" -lglfw3 -lgdi32
 if [ -e "Pong.exe" ]; then
     echo "File complication was successfully transferring the file to the bin."
-    mv Pong.exe "../bin"
+    mv Pong.exe "bin"
 else 
     read -p "File failed to be compiled. Read the error message."
 fi
