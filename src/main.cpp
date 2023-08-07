3a5bd375976d66f28cd26dd6dae2f284af82f821
RobertWalker0 <algorithmicrealmist@gmail.com>
2023-08-06 17:24:26 -0700

    Another iteration of main.
    The main file has been expanded for a program that checks if OpenGL
    successfully initialized on the system.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-
-    return 0;
+    if(!glfwInit())
+    {
+        std::cout << "OpenGL is not on the system." << std::endl;
+        system("pause");
+        return -1;
+    } else {
+        std::cout << "OpenGL is on the system" << std::endl;
+        system("pause");
+        return 0;
+    }
 }
