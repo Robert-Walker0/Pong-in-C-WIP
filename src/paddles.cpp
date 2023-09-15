6f2f7b4efa7078ecfc3fce3bbcd2e3aec89fa671
RobertWalker0 <algorithmicrealmist@gmail.com>
2023-09-15 12:37:23 -0700

    Refactor: Removes the if-statement for method Draw

--- src/paddles.cpp
+++ src/paddles.cpp
@@ -10,16 +10,14 @@ Paddle::Paddle(float x, float y)
 
 void Paddle::Draw(GLFWwindow *window)
 {
-    if(glfwInit())
-    {
-        glBegin(GL_QUADS);
-        glColor3f(1.0f, 1.0f, 1.0f);
-        glVertex2f(x, y);
-        glVertex2f(x + width, y);
-        glVertex2f(x + width, y + height);
-        glVertex2f(x, y + height);
-        glEnd();
-    }
+    
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f(x, y);
+    glVertex2f(x + width, y);
+    glVertex2f(x + width, y + height);
+    glVertex2f(x, y + height);
+    glEnd();
 }
 
 void Paddle::Translate(float dx, float dy)
