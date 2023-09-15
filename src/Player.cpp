f7eb4b11f9e0ef420a32902b8c960dc8db3deb10
RobertWalker0 <algorithmicrealmist@gmail.com>
2023-09-15 12:55:21 -0700

    Feat: Prevents the player from going out of bounds.
    The player can no longer go out of bounds of the screen while moving.

--- src/Player.cpp
+++ src/Player.cpp
@@ -9,11 +9,14 @@ Player::Player() : Paddle(-1.0, .1)
 
 void Player::keycallback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
-    if(key == GLFW_KEY_W || key == GLFW_KEY_UP)
+    if(key == GLFW_KEY_W || key == GLFW_KEY_UP && y < 0.6)
     {
+        //The y in the if is the top of the screen.
         this->Translate(0, moveSpeed);
-    } else if(key == GLFW_KEY_S || key == GLFW_KEY_DOWN)
+    } else if(key == GLFW_KEY_S || key == GLFW_KEY_DOWN && y > -1)
     {
+        //The y in this if is the bottom of the screen.
         this->Translate(0, -moveSpeed);
     } 
+    std::cout << "y=" << y << std::endl;
 }
