d3d0939e2fbf5d65fc2fdc4a8470b4cc2bcec2d4
RobertWalker0 <algorithmicrealmist@gmail.com>
2023-09-17 11:07:45 -0700

    Chore: Included path for stand alone file.
    Includes the path for the stand alone file to be processed by the
    compile just like GLFW. This is the path for the stb_image.h

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -I"deps/GLFW/include" src/*.cpp -o Pong.exe -L"deps/GLFW/lib-mingw-w64" -lglfw3 -lgdi32 -lopengl32
+g++ -I"deps/GLFW/include" -I"deps/stb/include" src/*.cpp -o Pong.exe -L"deps/GLFW/lib-mingw-w64" -lglfw3 -lgdi32 -lopengl32
 if [ -e "Pong.exe" ]; then
     echo "File complication was successfully transferring the file to the bin."
     mv Pong.exe "bin"
